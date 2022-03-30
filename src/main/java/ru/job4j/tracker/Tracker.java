d3eeae8bf6a5c8a7d9b50805d7c98f7ad044a8ff
Любовь <n.lubovj@gmail.com>
2022-03-30 18:19:57 +0300

    Add new ability in StartUI class: output item by id

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -30,16 +23,14 @@ public class Tracker {
     }
 
     public int indexOf(int id) {
-        int index = -1;
+        int result = -1;
         for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getId() == id) {
-                item.setId(id);
-                index = i;
+            if (items[i].getId() == id) {
+                result = i;
                 break;
             }
         }
-        return index;
+        return result;
     }
 
     public boolean replace(int id, Item item) {
