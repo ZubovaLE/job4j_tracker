613909ab84f630c7b781e805991c07ff475d3377
Любовь <n.lubovj@gmail.com>
2022-04-03 21:02:39 +0300

    Add tests into TrackerTest and LicenseTest classes

--- src/test/java/ru/job4j/pojo/LicenseTest.java
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.pojo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +8,58 @@ import static org.junit.jupiter.api.Assertions.*;
 class LicenseTest {
 
     @Test
+    @DisplayName("Test the equals method when names are equal")
     public void eqName() {
         License first = new License();
         first.setCode("audio");
         License second = new License();
         second.setCode("audio");
-        assertEquals(first, second);
+        boolean expected = first.equals(second);
+        assertTrue(expected);
+    }
+
+    @Test
+    @DisplayName("Test the equals method when owners are equal")
+    public void eqOwner() {
+        License first = new License();
+        first.setOwner("Ivanov");
+        License second = new License();
+        second.setOwner("Ivanov");
+        boolean expected = first.equals(second);
+        assertTrue(expected);
+    }
+
+    @Test
+    @DisplayName("Test the equals method when models are equal")
+    public void eqModel() {
+        License first = new License();
+        first.setModel("multi");
+        License second = new License();
+        second.setModel("multi");
+        boolean expected = first.equals(second);
+        assertTrue(expected);
+    }
+
+    @Test
+    @DisplayName("Test the equals method when created are equal")
+    public void eqCreated() {
+        Date date = new Date();
+        License first = new License();
+        first.setCreated(date);
+        License second = new License();
+        second.setCreated(date);
+        boolean expected = first.equals(second);
+        assertTrue(expected);
+    }
+
+    @Test
+    @DisplayName("Test the equals method when objects are not equal")
+    public void whenNotEqual() {
+        License first = new License();
+        first.setOwner("Petrov");
+        License second = new License();
+        second.setOwner("Ivanov");
+        boolean expected = first.equals(second);
+        assertFalse(expected);
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class TrackerTest {
+
     @Test
+    @DisplayName("Test the findById method when add new item")
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
         Item item = new Item();
@@ -14,5 +17,57 @@ public class TrackerTest {
         tracker.add(item);
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    @DisplayName("Test the findById method")
+    public void whenTestFindById() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        Item item = tracker.add(bug);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    @DisplayName("Test the findAll method")
+    public void whenTestFindAll() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        Item result = tracker.findAll()[0];
+        assertThat(result.getName(), is(first.getName()));
+    }
+
+    @Test
+    @DisplayName("Test the findByName method when check the first item name")
+    public void whenTestFindByNameCheckArrayLength() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findByName(first.getName());
+        assertThat(result.length, is(3));
+    }
+
+    @Test
+    @DisplayName("Test the findByName method when check the second item name")
+    public void whenTestFindByNameCheckSecondItemName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findByName(second.getName());
+        assertThat(result[1].getName(), is(second.getName()));
     }
 }
