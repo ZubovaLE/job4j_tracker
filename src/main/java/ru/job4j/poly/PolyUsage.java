1c2e95ab21428d46af04796b63a7c43e648cec6d
Любовь <n.lubovj@gmail.com>
2022-04-09 00:12:40 +0300

    Add Vehicle interface. Add AnotherBus, Train and Plane classes which implement the interface and override the move method. Add PolyUsage class with type-casting

---
+++ src/main/java/ru/job4j/poly/PolyUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.poly;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        Vehicle bus = new AnotherBus();
+        Vehicle plane = new Plane();
+        Vehicle train = new Train();
+
+        Vehicle[] vehicles = new Vehicle[]{bus, plane, train};
+        for (Vehicle vehicle : vehicles) {
+            vehicle.move();
+        }
+    }
+}
