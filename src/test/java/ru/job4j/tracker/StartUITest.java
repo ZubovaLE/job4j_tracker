3a2295c50eba77814477e7fc19d8c403ab42e95a
Любовь <n.lubovj@gmail.com>
2022-04-13 18:16:57 +0300

    Refactor StartUI class by using a Strategy template implementation. Add UserAction Interface and its implementations: CreateAction, ShowAction, EditAction, DeleteAction, FindByIdAction, FindByNameAction and ExitAction classes

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,46 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StartUITest {
 
     @Test
-    @DisplayName("Test creatItem method")
-    void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Tracker tracker = new Tracker();
-        Input input = new StubInput(answers);
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertEquals(created.getName(), expected.getName());
-    }
-
-    @Test
-    @DisplayName("Test editItem method")
-    void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-                "edited item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertEquals(edited.getName(), "edited item");
-    }
-
-    @Test
-    @DisplayName("Test deleteItem method")
-    void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        assertNull(tracker.findById(item.getId()));
-    }
-
-    @Test
+    @DisplayName("Test init method")
     void init() {
     }
 }
