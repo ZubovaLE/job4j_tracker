8caa42decd2b4bf2e55d74d8b0e40908ecd13a65
Любовь <n.lubovj@gmail.com>
2022-04-15 15:31:01 +0300

    Add Output interface to break the dependence on System.io. Add two implementations: ConsoleOutput and StubOutput classes. Refactor the project

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,48 +10,67 @@ class StartUITest {
     @Test
     @DisplayName("Test init method when add new item")
     void whenCreateItem() {
+        Output output = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new ExitAction()
+                new CreateAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertEquals(tracker.findAll()[0].getName(), "Item name");
     }
 
     @Test
     @DisplayName("Test init method when edit an item")
     void whenEditItem() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Edited item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), replacedName, "1"}
+                new String[]{"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
-                new EditAction(),
-                new ExitAction()
+                new EditAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertEquals(tracker.findById(item.getId()).getName(), replacedName);
     }
 
     @Test
     @DisplayName("Test init method when delete an item")
     void whenDeleteItem() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), "1"}
+                new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(),
-                new ExitAction()
+                new DeleteAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertNull(tracker.findById(item.getId()));
+    }
+
+    @Test
+    @DisplayName("Test init method when delete an item")
+    void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertEquals(out.toString(),
+                "Menu:" + System.lineSeparator() +
+                        "0. Exit Program" + System.lineSeparator() + "The program is finished. Thank you!" + System.lineSeparator()
+        );
     }
 }
