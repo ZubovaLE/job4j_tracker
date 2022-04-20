32dcf04e81887ed0a3704f0e3f58b0d5c7b2e73f
Любовь <n.lubovj@gmail.com>
2022-04-20 12:44:04 +0300

    Correct UserStore class with exceptions

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -3,20 +3,22 @@ package ru.job4j.ex;
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         boolean flag = true;
+        User rsl = null;
         for (User user : users) {
-            if (user.equals(login)) {
+            if (user.getUsername().equals(login)) {
                 flag = false;
+                rsl = user;
+                break;
             }
         }
-
         if (flag) {
             throw new UserNotFoundException("The user is not found");
         }
-        return null;
+        return rsl;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid()) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("The user is not valid");
         }
         return false;
@@ -26,12 +28,14 @@ public class UserStore {
         User[] users = {
                 new User("Petr Arsentev", true)
         };
-        User user = findUser(users, "Petr Arsentev");
         try {
+            User user = findUser(users, "Petr Arsentev");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
         } catch (UserInvalidException ex) {
+            ex.printStackTrace();
+        } catch (UserNotFoundException ex) {
             ex.printStackTrace();
         }
     }
