ea41db41e6542132f3b0f3921eb742d864ec7b7f
Любовь <n.lubovj@gmail.com>
2022-05-03 08:45:15 +0300

    Add tests for Find class

---
+++ src/test/java/ru/job4j/ex/FindTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.ex;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTest {
+
+    @Test
+    @DisplayName("Test get method when proper index = 0")
+    void testGetWhenProperIndexIs0() {
+        String[] in = {"one", "two", "three", "four", "five"};
+        int index = 0;
+        String out = Find.get(in, index);
+        String expected = "one";
+        assertEquals(expected, out);
+    }
+
+    @Test
+    @DisplayName("Test get method when proper index = 3")
+    void testGetWhenProperIndexIs3() {
+        String[] in = {"one", "two", "three", "four", "five"};
+        int index = 3;
+        String out = Find.get(in, index);
+        String expected = "four";
+        assertEquals(expected, out);
+    }
+
+    @Test
+    @DisplayName("Test get method when index equals array size then exception")
+    void testGetWhenIndexEqualsArraySizeThenException() {
+        String[] in = {"one", "two", "three", "four", "five"};
+        int index = 5;
+        Assert.assertThrows(IllegalArgumentException.class, () -> Find.get(in, index));
+    }
+
+    @Test
+    @DisplayName("Test get method when index is greater than array size then exception")
+    void testGetWhenIndexIsGreaterThanArraySizeThenException() {
+        String[] in = {"one", "two", "three", "four", "five"};
+        int index = 7;
+        Assert.assertThrows(IllegalArgumentException.class, () -> Find.get(in, index));
+    }
+
+    @Test
+    @DisplayName("Test get method when index = -3 then exception")
+    void testGetWhenIndexIsNegativeThenException() {
+        String[] in = {"one", "two", "three", "four", "five"};
+        int index = -3;
+        Assert.assertThrows(IllegalArgumentException.class, () -> Find.get(in, index));
+    }
+}
