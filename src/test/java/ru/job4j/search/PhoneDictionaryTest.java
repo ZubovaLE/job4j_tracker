021d8f6c21283e4d527563aecdb96d08199950b4
Любовь <n.lubovj@gmail.com>
2022-05-11 23:20:06 +0300

    Add test for PhoneDictionary class

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,18 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class PhoneDictionaryTest {
 
     @Test
-    void add() {
-    }
-
-    @Test
     @DisplayName("Test find when key is Petr")
-    public void whenFindByName() {
+    public void findWhenKeyIsPetr() {
         PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
+        phones.add(new Person("Petr", "Petrov", "534872", "Moscow"));
         ArrayList<Person> persons = phones.find("Petr");
-        assertEquals(persons.get(0).getSurname(), "Arsentev");
+        assertEquals("Petrov", persons.get(0).getSurname());
     }
 
     @Test
@@ -31,6 +25,47 @@ class PhoneDictionaryTest {
         phones.add(new Person("Ivan", "Ivanov", "12345", "Russia, Moscow"));
         phones.add(new Person("Petr", "Petrov", "54321", "Russia, Samara"));
         ArrayList<Person> persons = phones.find("Russia");
-        assertEquals(persons.get(1).getSurname(), "Petrov");
+        assertEquals("Petr", persons.get(1).getName());
+    }
+
+    @Test
+    @DisplayName("Test find when key is Samara")
+    void findWhenKeyIsSamara() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Ivan", "Ivanov", "12345", "Russia, Moscow"));
+        phones.add(new Person("Petr", "Petrov", "54321", "Russia, Samara"));
+        ArrayList<Person> persons = phones.find("Samara");
+        assertEquals("Petr", persons.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Test find when key is 12345")
+    void findWhenKeyIs12345() {
+        PhoneDictionary phones = new PhoneDictionary();
+        Person ivan = new Person("Ivan", "Ivanov", "12345", "Russia, Moscow");
+        Person petr = new Person("Petr", "Petrov", "54321", "Russia, Samara");
+        phones.add(ivan);
+        phones.add(petr);
+        ArrayList<Person> persons = phones.find("12345");
+        assertEquals(ivan, persons.get(0));
+    }
+
+    @Test
+    @DisplayName("Test find when key is Ru")
+    void findWhenKeyIsRu() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Ivan", "Ivanov", "12345", "Russia, Moscow"));
+        phones.add(new Person("Petr", "Petrov", "54321", "Russia, Samara"));
+        ArrayList<Person> persons = phones.find("Ru");
+        assertEquals(2, persons.size());
+    }
+
+    @Test
+    @DisplayName("Test find when not found then empty ArrayList")
+    void findWhenNotFoundThenEmptyArrayList() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Ivan", "Ivanov", "12345", "Russia, Moscow"));
+        ArrayList<Person> result = phones.find("Samara");
+        assertTrue(result.isEmpty());
     }
 }
