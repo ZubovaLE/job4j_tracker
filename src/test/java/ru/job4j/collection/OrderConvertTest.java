ddea095561d1efce913b2047895e6dec4d7716e3
Любовь <n.lubovj@gmail.com>
2022-05-22 23:52:27 +0300

    Add Order, OrderConvert classes with List to Map conversion and add test for OrderConvert class

---
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class OrderConvertTest {
+
+    @Test
+    @DisplayName("Test process method")
+    public void whenSingleOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+}
