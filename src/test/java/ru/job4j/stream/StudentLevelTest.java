21b559d4923ae698fecbcce57f2c2730c0d9ba8b
NikiforovaLE <n.lubovj@gmail.com>
2022-06-19 23:25:24 +0300

    Implement filtering of students on the score field by using Stream.takeWhile. Add tests for it

---
+++ src/test/java/ru/job4j/stream/StudentLevelTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class StudentLevelTest {
+
+    @Test
+    @DisplayName("Test when sorted")
+    public void whenSorted() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student(28, "Masha"));
+        input.add(new Student(128, "Petya"));
+        List<Student> expected = List.of(
+                new Student(128, "Petya"),
+                new Student(28, "Masha")
+        );
+        assertThat(StudentLevel.levelOf(input, 20), is(expected));
+    }
+
+    @Test
+    @DisplayName("Test when only null")
+    public void whenOnlyNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        List<Student> expected = List.of();
+        assertThat(StudentLevel.levelOf(input, 100), is(expected));
+    }
+
+    @Test
+    @DisplayName("Test when hash null")
+    public void whenHasNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        input.add(new Student(28, "Petya"));
+        List<Student> expected = List.of(new Student(28, "Petya"));
+        assertThat(StudentLevel.levelOf(input, 10), is(expected));
+    }
+}
