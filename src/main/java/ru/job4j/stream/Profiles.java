c4fa8e1aebd92a63c87958d00fd03bcb0c8a52cd
NikiforovaLE <n.lubovj@gmail.com>
2022-06-19 23:28:36 +0300

    Improve Profiles and School classes

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).toList();
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .toList();
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         profiles.sort(Comparator.comparing(profile -> profile.getAddress().getCity()));
-        return profiles.stream().map(Profile::getAddress).distinct().toList();
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .toList();
     }
 }
