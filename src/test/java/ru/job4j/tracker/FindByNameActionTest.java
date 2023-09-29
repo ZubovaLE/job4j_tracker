4c0836a3350b7dc5097ad334c2cd72d494e45e66
Lyubov <n.lubovj@gmail.com>
2023-09-29 23:56:00 +0300

    Add FindByNameActionTest class

---
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class FindByNameActionTest {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    @Test
+    @DisplayName("Test execute when successful")
+    void executeSuccessfully() {
+        Output out = new StubOutput();
+        Store tracker = new MemTracker();
+        String itemName = "Item";
+        Item item = new Item(itemName);
+        tracker.add(item);
+        FindByNameAction find = new FindByNameAction(out);
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any())).thenReturn(itemName);
+        find.execute(input, tracker);
+
+        String ln = System.lineSeparator();
+        String expected = "=== Find item by name ===" + ln + "id: " + item.getId() + ", name: " + itemName +
+                ", created: " + FORMATTER.format(item.getCreated()) + ln;
+
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    @DisplayName("Test execute when item is not found")
+    void executeWhenItemIsNotFound() {
+        Output out = new StubOutput();
+        Store tracker = new MemTracker();
+        tracker.add(new Item("Item"));
+        Input input = mock(Input.class);
+        String anotherItemName = "name";
+        when(input.askStr(any())).thenReturn(anotherItemName);
+        FindByNameAction find = new FindByNameAction(out);
+        find.execute(input, tracker);
+
+        String ln = System.lineSeparator();
+        String expected = "=== Find item by name ===" + ln + "Заявки с именем: " + anotherItemName + " не найдены." + ln;
+
+        assertThat(out.toString(), is(expected));
+    }
+}
